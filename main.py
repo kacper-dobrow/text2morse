b31321f699e3a9c7d8511b8b93fafc85be58d4ef
Kacper Dobrowolski <kacper.dobrow@gmail.com>
2024-06-20 10:09:28 +0200

    added the functionality

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from converter import text2morse
 if __name__=="__main__":
     print("Welcome to Text to Morse Translator!")
     phrase = input("Enter the phrase you would like translated:\n")
-    print(f"Your phrase in the Morse code\n{text2morse(phrase)}")
+    print(f"Your phrase in the Morse code:\n{text2morse(phrase)}")
